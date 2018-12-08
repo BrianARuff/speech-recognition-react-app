a16d3ac08b79f657266b57efbbb27eda882a19a7
BrianARuff <thelonewolf198927@gmail.com>
2018-12-08 13:55:42 -0500

    javascript speech api

---
+++ build/precache-manifest.17f22d35eeaa06e1bcfa84e935c7a689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3434f988307586f912a1",
+    "url": "/static/js/main.3434f988.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "3434f988307586f912a1",
+    "url": "/static/css/main.cc0b735a.chunk.css"
+  },
+  {
+    "revision": "4f50b1954b310a8ce0b230dccbeeea13",
+    "url": "/index.html"
+  }
+];
--- src/App.js
+++ src/App.js
@@ -1,28 +1,62 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    state = {
+        transcript: [],
+        confidence: [],
+        isRecording: false,
+    }
+
+    speechRecognition = () => {
+        if (window.hasOwnProperty("webkitSpeechRecognition")) {
+            const recognition = new window.webkitSpeechRecognition();
+            this.setState({isRecording: true});
+
+            recognition.continuous = true;
+            recognition.interimResults = false;
+
+            recognition.lang = "en-US";
+            recognition.start();
+            
+            recognition.onresult = (e) => {
+                console.log(e)
+                const new_transcript = this.state.transcript;
+                new_transcript.push(e.results[0][0].transcript);
+                const new_confidence = this.state.confidence;
+                new_confidence.push((e.results[0][0].confidence * 100).toFixed(2) + "%");
+                this.setState({transcript: new_transcript, confidence: new_confidence});
+                recognition.stop();
+                this.setState({isRecording: false});
+            };
+
+            recognition.onerror = () => {
+                recognition.stop();
+                this.setState({isRecording: false});
+            };
+        } else {
+            console.log("Use most recent version of Chrome.")
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.transcript.map((word, index) => {
+                        return (
+                            <React.Fragment key={Math.random() * Date.now()}>
+                                <p>{word}, Confidence: {this.state.confidence[index]}</p>
+                            </React.Fragment>
+                        )
+                    })
+                }
+                <button className="speech-recognition-button" onClick={this.speechRecognition}>Click to Analyse Speech
+                </button>
+                {this.state.isRecording ? <p>Analyzing Speech...</p> : null }
+            </div>
+        );
+    }
 }
 
 export default App;
